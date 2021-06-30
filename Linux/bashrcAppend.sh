64248f67c3c14534b41303edb8b8ee2ca9892106
Charles Vardeman <charles.vardeman@gmail.com>
2021-06-30 17:15:12 -0400

    Adding updated alias

--- Linux/bashrcAppend.sh
+++ Linux/bashrcAppend.sh
@@ -7,7 +7,7 @@ alias ls='ls -v --color=auto'
 alias ll='ls -l' # overwrite Ubuntu's ll
 alias l='ls -CF'
 alias l1='l -1'
-alias tmux="TERM=xterm-256color $HOME/local/bin/tmux"
+alias tmux="TERM=xterm-256color $HOME/bin/tmux"
 alias vim='TERM=xterm-256color /usr/bin/vim'
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
@@ -18,7 +18,6 @@ alias pl='ip --pylab'
 alias pip-update="pip install --upgrade pip && pip freeze --local | grep -v \
 '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U"
 alias py="python"
-alias weechat="$HOME/local/bin/weechat"
 alias PPUU="conda activate PPUU"
 
 # Highlight numbers when displaying text files
@@ -67,43 +66,24 @@ fi
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH':/usr/local/cuda/lib64'
 
 # Shortcuts and aliases for NYU and CIMS machines ##############################
-#Some useful EXPORTs
-export NYU='nyu.edu'
-export CIMS='cims.'$NYU
-export CS='cs.'$NYU
-export L1='lion1.'$CS
-export L2='lion2.'$CS
-export L3='lion3.'$CS
-export L4='lion4.'$CS
-export L5='lion5.'$CS
-export L6='lion6.'$CS
-export ACCESS="access.$CIMS"
-export MYBOX="box795.$CIMS"
-export CASSIO="cassio."$CS
 
-# and corresponding ALIAs
-alias L1="ssh $L1"
-alias L2="ssh $L2"
-alias L3="ssh $L3"
-alias L4="ssh $L4"
-alias L5="ssh $L5"
-alias L6="ssh $L6"
-alias Access="ssh $ACCESS"
-alias MyBox="ssh $MYBOX"
-alias Cassio="ssh $CASSIO"
+#Some useful EXPORTs
+# Common machines
+export ND='nd.edu'
+export CRC='crc.'$ND
+export FE1='crcfe01.'$CRC
+export FE2='crcfe02.'$CRC
+export CAML='caml.'$CRC
+
+# and corresponding ALIASes
+alias FE1="ssh $FE1"
+alias FE2="ssh $FE2"
+alias CAML="ssh $CAML"
 
 # with X forwarding
-alias L1X="ssh $L1 -Y"
-alias L2X="ssh $L2 -Y"
-alias L3X="ssh $L3 -Y"
-alias L4X="ssh $L4 -Y"
-alias L5X="ssh $L5 -Y"
-alias L6X="ssh $L6 -Y"
-alias AccessX="ssh -Y $ACCESS"
-alias MyBoxX="ssh -Y $MYBOX"
-alias Cassio="ssh -Y $CASSIO"
+alias FE1X="ssh -Y $FE1"
 
-alias Gpu="srun --qos=interactive --gres=gpu:1 --exclude=rose[1-9] --pty bash"
+# alias Gpu="srun --qos=interactive --gres=gpu:1 --exclude=rose[1-9] --pty bash"
 
 # Run once: $ jupyter notebook password
 function nb {
